86abfefd8102c1254ee5349713e742fafe13b7d0
KerronKing <king_of_palmiste@hotmail.com>
2020-04-10 10:04:20 -0400

    feat: create new gatsby plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,22 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import reducer from '../reducer';
-import Search from '../components/search-form';
+// import reducer from '../reducer';
+// import Search from '../components/search-form';
 
-const initialState = { recipe: {} };
+// const initialState = { recipe: {} };
 
-const IndexPage = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return (
-    <Layout>
-      <SEO title="Home" />
-      <h1>Recipe Depot</h1>
-      <p>If you need a killer recipe, look no futher.</p>
-      <Search onclick={() => dispatch()} />
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
-  );
-};
+const IndexPage = () => (
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  <Layout>
+    <SEO title="Home" />
+    <h1>Recipe Depot</h1>
+    <p>If you need a killer recipe, look no futher.</p>
+    {/* <Search onclick={() => dispatch()} /> */}
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+);
 
 export default IndexPage;
